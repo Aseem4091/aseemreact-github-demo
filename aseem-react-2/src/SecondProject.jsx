8191fa0b647b81268d5caf33f7a978412268d8f3
Aseem4091 <https://github.com/sagun03/furnishFantasy-ui.git>
2024-10-11 09:36:13 -0400

    Made chanegs to the file

--- aseem-react-2/src/SecondProject.jsx
+++ aseem-react-2/src/SecondProject.jsx
@@ -5,6 +5,7 @@ function SecondProject() {
     <div>
         <p> This is my second Project I am doing it</p>
         <p>I am doing devops assignment</p>
+        <p>I am adding the third line for workflow.</p>
         </div>
   )
 }
